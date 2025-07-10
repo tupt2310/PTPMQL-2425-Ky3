927fc454dab8022b2aba4f3a0402793edc6b46f4
tupt2310 <phamthanhtu0701@gmail.com>
2025-07-10 13:53:43 +0700

    BTH_0907_sinhIdTudong

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -4,19 +4,56 @@ using MvcMovie.Model;
 namespace MvcMovie.Controllers
 {
     public class PersonController : Controller
-
     {
+        private readonly Data.ApplicationDbContext _context;
+
+        public PersonController(Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        // GET: Person/Create
+        public IActionResult Create()
+        {
+            // Sinh mã PersonId tự động
+            int count = _context.Persons.Count();
+            string newPersonId = $"PS{(count + 1).ToString("D3")}";
+            var person = new MvcMovie.Model.Person { PersonId = newPersonId };
+            return View(person);
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MvcMovie.Model.Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Persons.Add(person);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+
         [HttpPost]
         public IActionResult Index(Person ps)
         {
-    string strOutput = "Xin chào " + ps.PersonId + " - " + ps.FullName + " - " + ps.Address;
-    ViewBag.infoPerson = strOutput;
-    return View();
+            // Nếu PersonId chưa có, tự động sinh mã mới
+            if (string.IsNullOrEmpty(ps.PersonId))
+            {
+                int count = _context.Persons.Count();
+                ps.PersonId = $"PS{(count + 1).ToString("D3")}";
+            }
+            string strOutput = "Xin chào " + ps.PersonId + " - " + ps.FullName + " - " + ps.Address;
+            ViewBag.infoPerson = strOutput;
+            return View();
         }
     }
 }
--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e83f66ed5321ab3c597fe66a75064737f5aa505")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
